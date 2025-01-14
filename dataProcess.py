b6bb9e38b2021358b58ef6312942d1fa928823f8
piyush dixit <dixitpiyushk@gmail.com>
2025-01-14 16:56:22 +0530

    added graphs

--- dataProcess.py
+++ dataProcess.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
 from pyspark.sql.functions import col
-
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 spark = SparkSession.builder.appName("read 1 crore data").config("spark.jars",
                                                                  r"C:\Users\dixit\PycharmProjects\pythonProgram\HealthHarvest\mysqlJar\mysql-connector-j-9.1.0.jar").getOrCreate()
@@ -26,32 +27,30 @@ rdd = rdd.withColumnRenamed("Name", "Patient_Name") \
     .withColumnRenamed("Medication", "Prescribed_Medication") \
     .withColumnRenamed("Test Results", "Test_Results")
 
-filtered_df = rdd.filter((col("Medical_Condition") == "Diabetes") & (col("Patient_Age") < 30))
-filtered_df = rdd.filter((col("Date_of_Admission") >= "2020-01-01") & (col("Date_of_Admission") <= "2021-01-01"))
 
-# Only_Age_RDD = filtered_df.select("Patient_Age")
-# Only_Date_Of_Admission_RDD = filtered_df.select("Date_of_Admission")
-#
-# age_data = Only_Age_RDD.collect()
-# ages = [row["Patient_Age"] for row in age_data]
-#
-#
-# Date_Of_Admission_data = Only_Date_Of_Admission_RDD.collect()
-# Date_Of_Admission = [row["Date_of_Admission"] for row in Date_Of_Admission_data]
-#
-#
-#
-# x_axis = range(len(ages))
-# plt.figure(figsize=(10, 6))
-# plt.plot(x_axis, ages, marker="o", linestyle="-", color="b", label="Patient Age")
-#
-# plt.xlabel("Record Index")
-# plt.ylabel("Patient Age")
-# plt.title("Patient Age for Diabetes Patients Under 30")
-# plt.legend()
-# plt.grid(True)
-#
-# plt.show()
+#piechart diabetes patient accroding to the ages
+filtered_df = rdd.filter(rdd['Medical_Condition'] == 'Diabetes')
+gender_counts = filtered_df.groupBy("Patient_Gender").count().collect()
+labels = [row['Patient_Gender'] for row in gender_counts]
+sizes = [row['count'] for row in gender_counts]
+plt.figure(figsize=(6, 6))
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=['blue', 'pink'])
+plt.title('Diabetes Patient Gender Distribution')
+plt.show()
+
+
+#ba chart on obesity
+ObesityDF = rdd.filter((rdd['Medical_Condition'] == 'Obesity') & (rdd['Patient_Age'] > 25))
+gender_counts = (ObesityDF.groupBy("Patient_Gender").count().collect())
+labels = [row['Patient_Gender'] for row in gender_counts]
+counts = [row['count'] for row in gender_counts]
+plt.figure(figsize=(8, 6))
+plt.bar(labels, counts, color=['blue', 'pink'])
+plt.title('Obesity Cases by Gender whose age is less than 25')
+plt.xlabel('Gender')
+plt.ylabel('Count')
+plt.show()
+
 
 # databaseName = "pysparkproject"
 # jdbc_url = f"jdbc:mysql://localhost:3306/{databaseName}"
