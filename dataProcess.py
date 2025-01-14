c7e6d65048671f9aa22492c201840007bfadaf1c
piyush dixit <dixitpiyushk@gmail.com>
2025-01-14 18:56:54 +0530

    added the grphs

--- dataProcess.py
+++ dataProcess.py
@@ -1,14 +1,15 @@
 from pyspark.sql import SparkSession
+from matplotlib.ticker import FuncFormatter
 import matplotlib.pyplot as plt
-from pyspark.sql.functions import col
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+import pandas as pd
+from pyspark.sql import functions as F
+from pyspark.sql import functions as F
+
 
 spark = SparkSession.builder.appName("read 1 crore data").config("spark.jars",
                                                                  r"C:\Users\dixit\PycharmProjects\pythonProgram\HealthHarvest\mysqlJar\mysql-connector-j-9.1.0.jar").getOrCreate()
 
-filepath = r"C:/Users/dixit/PycharmProjects/pythonProgram/mydata.csv"
+filepath = r"C:/Users/dixit/PycharmProjects/pythonProgram/20kdata.csv"
 
 rdd = spark.read.csv(filepath, header=True, inferSchema=True)
 
@@ -25,7 +26,9 @@ rdd = rdd.withColumnRenamed("Name", "Patient_Name") \
     .withColumnRenamed("Admission Type", "Admission_Type") \
     .withColumnRenamed("Discharge Date", "Discharge_Date") \
     .withColumnRenamed("Medication", "Prescribed_Medication") \
-    .withColumnRenamed("Test Results", "Test_Results")
+    .withColumnRenamed("Test Results", "Test_Results") \
+    .withColumnRenamed("city", "city")
+
 
 
 #piechart diabetes patient accroding to the ages
@@ -39,17 +42,41 @@ plt.title('Diabetes Patient Gender Distribution')
 plt.show()
 
 
-#ba chart on obesity
-ObesityDF = rdd.filter((rdd['Medical_Condition'] == 'Obesity') & (rdd['Patient_Age'] > 25))
-gender_counts = (ObesityDF.groupBy("Patient_Gender").count().collect())
-labels = [row['Patient_Gender'] for row in gender_counts]
-counts = [row['count'] for row in gender_counts]
-plt.figure(figsize=(8, 6))
-plt.bar(labels, counts, color=['blue', 'pink'])
-plt.title('Obesity Cases by Gender whose age is less than 25')
-plt.xlabel('Gender')
-plt.ylabel('Count')
+
+#plot the bar graph for number of cities and patient avaliable in the coresponding city
+city_patient_counts = rdd.groupBy("city").agg(F.count("Patient_Name").alias("Patient_Count"))
+city_patient_counts_list = city_patient_counts.collect()
+cities = [row['city'] for row in city_patient_counts_list]
+patient_counts = [row['Patient_Count'] for row in city_patient_counts_list]
+plt.figure(figsize=(10, 6))
+plt.bar(cities, patient_counts, color='skyblue')
+plt.xlabel('City')
+plt.ylabel('Number of Patients')
+plt.title('Number of Patients by City')
+plt.xticks(rotation=45)
 plt.show()
+
+
+
+#plot the line graph
+data = rdd.collect()
+pandas_df = pd.DataFrame(data, columns=rdd.columns)
+pandas_df['Date_of_Admission'] = pd.to_datetime(pandas_df['Date_of_Admission'])
+pandas_df['Year_of_Admission'] = pandas_df['Date_of_Admission'].dt.year
+billing_per_year = pandas_df.groupby('Year_of_Admission')['Billing_Amount'].sum()
+def currency_format(x, pos):
+    return '₹{:,.2f}'.format(x)
+plt.figure(figsize=(10, 6))
+billing_per_year.plot(kind='line', marker='o', color='blue')
+plt.title('Total Billing Amount per Year')
+plt.xlabel('Year')
+plt.ylabel('Total Billing Amount')
+plt.gca().yaxis.set_major_formatter(FuncFormatter(currency_format))
+plt.grid(True)
+plt.xticks(billing_per_year.index)
+plt.show()
+
+
 
 
 # databaseName = "pysparkproject"
